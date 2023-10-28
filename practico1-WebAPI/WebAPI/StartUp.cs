735e0821abc8f70478bc8ac983063f24eb22bd4c
leonardosellanes <pedritodiestro@gmail.com>
2023-10-28 13:51:58 -0300

    v 1.0

--- practico1-WebAPI/WebAPI/StartUp.cs
+++ practico1-WebAPI/WebAPI/StartUp.cs
@@ -31,50 +31,61 @@ namespace WebAPI
                 using (var roleManager = serviceScope.ServiceProvider.GetService<RoleManager<IdentityRole>>())
                 {
                     #region Agregamos Los Roles Por defecto
-                    //var userRole = await roleManager.FindByIdAsync("ADMIN");
-                    //if (userRole == null)
-                    //{
-                    //    IdentityRole identityRole = new IdentityRole();
-                    //    identityRole = new IdentityRole();
-                    //    identityRole.Id = "ADMIN";
-                    //    identityRole.NormalizedName = "ADMIN";
-                    //    identityRole.Name = "ADMIN";
-                    //    identityRole.ConcurrencyStamp = "ADMIN";
-                    //    await roleManager.CreateAsync(identityRole);
-                    //}
+                    var userRole = await roleManager.FindByIdAsync("ADMIN");
+                    if (userRole == null)
+                    {
+                        IdentityRole identityRole = new IdentityRole();
+                        identityRole = new IdentityRole();
+                        identityRole.Id = "ADMIN";
+                        identityRole.NormalizedName = "ADMIN";
+                        identityRole.Name = "ADMIN";
+                        identityRole.ConcurrencyStamp = "ADMIN";
+                        await roleManager.CreateAsync(identityRole);
+                    }
 
-                    //userRole = await roleManager.FindByIdAsync("OTRO");
-                    //if (userRole == null)
-                    //{
-                    //    IdentityRole identityRole = new IdentityRole();
-                    //    identityRole = new IdentityRole();
-                    //    identityRole.Id = "OTRO";
-                    //    identityRole.NormalizedName = "OTRO";
-                    //    identityRole.Name = "OTRO";
-                    //    identityRole.ConcurrencyStamp = "OTRO";
-                    //    await roleManager.CreateAsync(identityRole);
-                    //}
+                    userRole = await roleManager.FindByIdAsync("MANAGER");
+                    if (userRole == null)
+                    {
+                        IdentityRole identityRole = new IdentityRole();
+                        identityRole = new IdentityRole();
+                        identityRole.Id = "MANAGER";
+                        identityRole.NormalizedName = "MANAGER";
+                        identityRole.Name = "MANAGER";
+                        identityRole.ConcurrencyStamp = "MANAGER";
+                        await roleManager.CreateAsync(identityRole);
+                    }
 
+                    userRole = await roleManager.FindByIdAsync("USER");
+                    if (userRole == null)
+                    {
+                        IdentityRole identityRole = new IdentityRole();
+                        identityRole = new IdentityRole();
+                        identityRole.Id = "USER";
+                        identityRole.NormalizedName = "USER";
+                        identityRole.Name = "USER";
+                        identityRole.ConcurrencyStamp = "USER";
+                        await roleManager.CreateAsync(identityRole);
+                    }
                     #endregion
 
                     #region Agregamos el usuario admin por defecto.
 
                     // Agregamos el usuario administrador por defecto si no exsite.
-                    //var userAdmin = await userManager.FindByEmailAsync("admin@admin.com");
-                    //if (userAdmin == null)
-                    //{
-                    //    Usuarios user = new Usuarios()
-                    //    {
-                    //        Email = "admin@admin.com",
-                    //        SecurityStamp = Guid.NewGuid().ToString(),
-                    //        UserName = "admin"
-                    //    };
+                    var userAdmin = await userManager.FindByEmailAsync("admin@admin.com");
+                    if (userAdmin == null)
+                    {
+                        ApplicationUser user = new ApplicationUser()
+                        {
+                            Email = "admin@admin.com",
+                            SecurityStamp = Guid.NewGuid().ToString(),
+                            UserName = "admin"
+                        };
 
-                    //    var result = await userManager.CreateAsync(user, "Abc*123!");
+                        var result = await userManager.CreateAsync(user, "Abc*123!");
 
-                    //    // Asignar Rol ADMIN
-                    //    await userManager.AddToRoleAsync(user, "ADMIN");
-                    //}
+                        // Asignar Rol ADMIN
+                        await userManager.AddToRoleAsync(user, "ADMIN");
+                    }
 
                     #endregion
                 }
