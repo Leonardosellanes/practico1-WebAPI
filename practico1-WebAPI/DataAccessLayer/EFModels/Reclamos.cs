7fc8daf6fe8e69914662327458f8c4b3cc1f93f2
leonardosellanes <pedritodiestro@gmail.com>
2023-11-10 11:08:16 -0300

    Reclamos y factura mock

---
+++ practico1-WebAPI/DataAccessLayer/EFModels/Reclamos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EFModels
+{
+    public class Reclamos
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(128), MinLength(3), Required]
+        public string Descripcion { get; set; } = "";
+        [Required]
+        public DateTime Fecha{ get; set; }
+        public int EmpresaId { get; set; }
+
+        [ForeignKey("EmpresaId")]
+        public Empresas? EmpresaAsociada { get; set; }
+    }
+}
---
+++ practico1-WebAPI/FacturaMock/FacturaMock/Models/RequestData.cs
@@ -0,0 +1,9 @@
+﻿namespace FacturaMock.Models
+{
+    public class RequestData
+    {
+        public string FechaInicio { get; set; } = string.Empty;
+        public string FechaFin { get; set; } = string.Empty;
+        public string ComisionesTotales { get; set; } = string.Empty;
+    }
+}
